400db332e436babe92ffe01f0e693a04a3187f01
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-10 14:07:38 -0800

    Feed data to job list

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,18 +1,22 @@
+import jobs from '../../data/jobs.json'
+
 const JobList = () => {
   return (
     <>
       <ul id="jobList">
-        <li>
-          <a href="/#/jobs/1" className="job">
-            <div className="logoWrapper" style={{ backgroundColor: 'hsla(157, 56%, 50%, 1)' }}>
-              <img src="/logos/crowdfund.svg" alt="Crowdfund" />
-            </div>
-            <span>5h ago<i className="bullet"></i>Full Time</span>
-            <h3>Senior Software Engineer</h3>
-            <span>CrowdFund</span>
-            <small>United Kingdom</small>
-          </a>
-        </li>
+        {jobs.map(j => (
+          <li key={j.id}>
+            <a href="/#/jobs/1" className="job">
+              <div className="logoWrapper" style={{ backgroundColor: j.logoBackground }}>
+                <img src={j.logo} alt={j.company} />
+              </div>
+              <span>{j.postedAt}<i className="bullet"></i>{j.contract}</span>
+              <h3>{j.position}</h3>
+              <span>{j.company}</span>
+              <small>{j.location}</small>
+            </a>
+          </li>
+        ))}
       </ul>
 
       <button id="loadMoreButton" className="button">Load More</button>
