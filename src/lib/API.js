9e8e4bf53efed579358ab016578290a5ad6644d3
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-12 07:32:58 -0800

    retrieve a single job from the api

--- src/lib/API.js
+++ src/lib/API.js
@@ -4,3 +4,8 @@ export async function getJobList() {
   return fetch(`${API_HOST}/jobs/`)
     .then(response => response.json())
 }
+
+export async function getSingleJob(jobId) {
+  return fetch(`${API_HOST}/jobs/${jobId}`)
+    .then(response => response.json())
+}
