e3fc57ef9adc6e136022b4775f06dad648fc3b17
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-12 11:05:31 -0800

    disable vitest browser -- too slow

--- vitest.config.js
+++ vitest.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    browser: {
+    /*browser: {
       enabled: true,
       headless: true,
       name: "chrome"
-    }
+    }*/
   }
 })
