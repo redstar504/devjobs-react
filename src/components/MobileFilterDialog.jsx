2d12fe016d8d87410638e281f9a210d8b423ba90
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-12 17:24:13 -0800

    mobile locality suggestions

--- src/components/MobileFilterDialog.jsx
+++ src/components/MobileFilterDialog.jsx
@@ -1,46 +1,100 @@
-import { useState } from 'react'
-import { FaStar } from 'react-icons/fa'
-import { FaLocationCrosshairs } from 'react-icons/fa6'
+import { useEffect, useReducer, useState } from 'react'
+import { FaLocationCrosshairs, FaLocationDot } from 'react-icons/fa6'
+import { getPlaceRecommendations } from '../lib/places.js'
 
 const MobileFilterDialog = ({ onClose = f => f }) => {
-  const [locationQuery, setLocationQuery] = useState("")
+  const [locationQuery, setLocationQuery] = useState('')
   const [isSuggestionsOpen, setIsSuggestionsOpen] = useState(false)
+  const [searchParams, setSearchParams] = useState({
+    lat: undefined,
+    lng: undefined,
+    placeId: undefined,
+    fullTimeOnly: false,
+    query: ""
+  })
+  const [isSearchingCurrentLocation, setIsSearchingCurrentLocation] = useState(false)
+  const [searchSuggestions, setSearchSuggestions] = useState([])
 
-  const handleLocationQuery = (query) => {
+  const handleSubmitSearch = e => {
+    e.preventDefault()
+    console.log(searchParams);
+  }
+
+  const handleTypingLocation = (query) => {
+    if ('' === query) {
+      setSearchSuggestions([])
+    }
     setIsSuggestionsOpen(true)
-    setLocationQuery(query)
+    setSearchParams({...searchParams, query: query, placeId: undefined})
+    getPlaceRecommendations(query, results => {
+      const mappedCities = results.map(({ structured_formatting: { main_text: city }, place_id }) => ({
+        city,
+        place_id
+      })).slice(0, 3)
+      setSearchSuggestions(mappedCities)
+    })
   }
 
   const handleUseMyLocation = () => {
-    const geo = Navigator.geolocation;
-    console.log(geo)
+    const success = f => {
+      setSearchParams({
+        ...searchParams,
+        query: 'My Current Location',
+        lat: f.coords.latitude,
+        lng: f.coords.longitude
+      })
+      setIsSearchingCurrentLocation(false)
+      setIsSuggestionsOpen(false)
+    }
+
+    navigator.geolocation.getCurrentPosition(success, () => {
+      alert('Geolocation unavailable, or has been declined by device settings.')
+    })
   }
+
+  const handleSelectSuggestion = i => e => {
+    e.preventDefault()
+    setSearchParams({...searchParams, query: searchSuggestions[i].city, placeId: searchSuggestions[i].place_id})
+    setIsSuggestionsOpen(false)
+  }
+
+  const toggleFullTimeOnly = () => setSearchParams({...searchParams, fullTimeOnly: !searchParams.fullTimeOnly})
 
   return (
     <>
-      <div id="mobileFiltersWrapper">
-        <input
-          className="textField"
-          placeholder="Filter by location..."
-          onChange={e => handleLocationQuery(e.target.value)}
-          value={locationQuery}
-        />
-        { isSuggestionsOpen && (
-          <ul id="mobileLocationSuggestions">
-            <li>
-              <a href="#" onClick={handleUseMyLocation}>
-                <FaLocationCrosshairs /> Use my location
-              </a>
-            </li>
-          </ul>
-        )}
-        <label id="mobileFullTimeQueryLabel">
-          <input type="checkbox" id="mobileFullTimeQuery" />
-          Full Time Only
-        </label>
-        <label htmlFor="areMobileFiltersOpen" id="mobileFilterButton" className="button">Search</label>
+      <div id="mobileFiltersContainer" onClick={onClose}>
+        <div id="mobileFiltersWrapper" onClick={e => e.stopPropagation()}>
+          <input
+            className="textField"
+            placeholder="Filter by location..."
+            onChange={e => handleTypingLocation(e.target.value)}
+            value={searchParams.query}
+            disabled={isSearchingCurrentLocation}
+          />
+          {isSuggestionsOpen && (
+            <ul id="mobileLocationSuggestions">
+              <li>
+                <a href="#" onClick={handleUseMyLocation}>
+                  <FaLocationCrosshairs /> Use current location
+                </a>
+              </li>
+              {searchSuggestions.length > 0 && searchSuggestions.map((s, i) => (
+                <li key={i}>
+                  <a href="#" onClick={handleSelectSuggestion(i)}>
+                    <FaLocationDot />{s.city}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+          <label id="mobileFullTimeQueryLabel">
+            <input type="checkbox" id="mobileFullTimeQuery" checked={searchParams.fullTimeOnly}
+                   onChange={toggleFullTimeOnly} />
+            Full Time Only
+          </label>
+          <button id="submitMobileSearch" className="button" onClick={handleSubmitSearch}>Search</button>
+        </div>
       </div>
-      <div id="overlay" onClick={onClose}></div>
     </>
   )
 }
--- src/lib/places.js
+++ src/lib/places.js
@@ -1,13 +1,23 @@
-// todo: restrict maps api key
-
 const KEY = import.meta.env.VITE_GMAPS_API_KEY
+import { Loader } from '@googlemaps/js-api-loader'
 
-export function getPlaceRecommendations(query) {
-  fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&key=${KEY}`)
-    .then(res => res.json())
-    .then(json => {
-      console.log(json)
+const loader = new Loader({
+  apiKey: import.meta.env.VITE_GMAPS_API_KEY,
+  version: 'weekly',
+  libraries: ['places']
+})
+
+export function getPlaceRecommendations(query, onResults) {
+  loader
+    .importLibrary('places')
+    .then(({ AutocompleteService }) => {
+      const service = new AutocompleteService()
+      service.getPlacePredictions({
+        input: query,
+        types: ['locality'],
+      }, onResults)
     })
+    .catch((e) => {
+      console.error(e)
+    });
 }
-
-getPlaceRecommendations("Van")
