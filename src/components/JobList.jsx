568534a2f7e61939d06d324eabf3fa82cf6898fd
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-15 18:07:15 -0800

    show currently applied filters

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -2,14 +2,25 @@ import { useJobList } from '../hooks/useJobList.jsx'
 import { JobListItem } from './JobListItem.jsx'
 
 const JobList = () => {
-  const { jobs, nextPage, hasMoreResults } = useJobList()
+  const { jobs, nextPage, hasMoreResults, hasActiveFilters, filters } = useJobList()
 
-  return jobs.length > 0 && (
+  return (
     <>
-      <ul id="jobList">
-        {jobs.map((j, i) => <JobListItem key={i} job={j} />)}
-      </ul>
-      {hasMoreResults && <button id="loadMoreButton" className="button" onClick={nextPage}>Load More</button>}
+      {hasActiveFilters && (
+        <ul id="activeFilterList">
+          {filters.lat && filters.lng && (<li>Within 1000 km</li>)}
+          {filters.locationQuery && (<li>{filters.locationQuery}</li>)}
+          {filters.fullTimeOnly && (<li>Full Time</li>)}
+        </ul>
+      )}
+      {jobs.length > 0 && (
+        <>
+          <ul id="jobList">
+            {jobs.map((j, i) => <JobListItem key={i} job={j} />)}
+          </ul>
+          {hasMoreResults && <button id="loadMoreButton" className="button" onClick={nextPage}>Load More</button>}
+        </>
+      )}
     </>
   )
 }
